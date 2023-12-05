e3dd6b084c4641a6dba30a71ffaf216d2eea299f
Bas Borgs <basborgs@msn.com>
2023-12-05 17:41:42 +0100

    3 december deel 2

--- 2023/3 december/Deel 2/AdventOfCode_20231203_2.py
+++ 2023/3 december/Deel 2/AdventOfCode_20231203_2.py
@@ -1,34 +1,55 @@
 #'''import pandas as pd # Library to work with datasets
 
-data_bestand = open("3 december/Data/Input_20231203.txt")
+data_bestand = open("2023/3 december/Data/Input_20231203.txt")
 data = data_bestand.read().split('\n') # Use split on \n instead of readlines(). This makes th \n dissapear 
+#data = data[:3]
 lengthDataset = len(data) # gebruik om niet buiten de boundries te gaan
 widthLines = len(data[0])
 #'''
 
+# First find the '*' symbols for every * symbol we will search for the 
+# numbers around if only two, multiply and at the '*'-symbol to the list
+
+    
+
 # This is a function that (if we find a number) will find the whole number and will give where it is placed 
 def findNumber(sLineNumber, sPoint):
     sLine = data[sLineNumber-1]
-    sContinue = 1
-    sNumber = sLine[sPoint]
+    sContinue1 = 1
+    sContinue2 = 1
 
-    while sContinue == 1:
-        if sPoint+1 < widthLines:
-            if sLine[sPoint+1].isdigit():
-                sPoint += 1
-                sNumber += sLine[sPoint]
-                sContinue = 1
+    # Finding starting position number
+    sStartPoint = sPoint
+    while sContinue1 == 1:
+        if sStartPoint-1 >= 0:
+            if sLine[sStartPoint-1].isdigit():
+                sStartPoint -= 1
+                sContinue1 = 1
             else:
-                sEndNumberPos = sPoint
-                sContinue = 0
+                sContinue1 = 0
         else:
-            sEndNumberPos = sPoint
-            sContinue = 0
-    return int(sNumber), sEndNumberPos, sPoint
+            sContinue1 = 0
 
-# In this function given the position of a number we will find if there is a symbol adjacent
+    sNumber = sLine[sStartPoint]
+    # Finding whole number using new starting position number
+    while sContinue2 == 1:
+        if sStartPoint+1 < widthLines:
+            if sLine[sStartPoint+1].isdigit():
+                sStartPoint += 1
+                sNumber += sLine[sStartPoint]
+                sContinue2 = 1
+            else:
+                sEndNumberPos = sStartPoint
+                sContinue2 = 0
+        else:
+            sEndNumberPos = sStartPoint
+            sContinue2 = 0
+    #print(sNumber)
+    return int(sNumber), sStartPoint, sEndNumberPos
+
+# In this function given the position of a number we will itterate all the neighbours
 def findSymbol(sLineNumber, sStartNumberPos, sEndNumberPos):
-    
+
     # Some alterations to the input data so secure it stays inside of the array
     if sStartNumberPos == 0:
         sStartNumberPos = 1
@@ -36,13 +57,13 @@ def findSymbol(sLineNumber, sStartNumberPos, sEndNumberPos):
     if sEndNumberPos == widthLines-1:
         sEndNumberPos = widthLines-2
 
-    # Look at the previous line if there is a symbol
+    # Look at the previous line
     if sLineNumber > 1:
         sLinePrevious = data[sLineNumber-2]
         
         for sPoint in sLinePrevious[sStartNumberPos-1:sEndNumberPos+2]: #sEndNumberPos + 1 doesnt include the last one here
             if sPoint != '.' and not sPoint.isdigit():
-                return True
+                return True                
     
     # Look in current line if there is a symbol
     sLine = data[sLineNumber-1]
@@ -63,7 +84,58 @@ def findSymbol(sLineNumber, sStartNumberPos, sEndNumberPos):
     # if the function hasn't stopped yet, return false, then there is no symbol adjacent to the number    
     return False
 
+# In this function we search for all the numbers around a star symbol
+def findNumbersStar(sLineNumber, sStarPos):
+    
+    sNumbers = []
 
+    # Some alterations to the input data so secure it stays inside of the array
+    if sStarPos == 0:
+        sStartStarPos = 1
+        sEndStarPos = 0
+    else:
+        sStartStarPos = sStarPos
+        sEndStarPos = sStarPos
+
+    # Previous line:
+    if sLineNumber > 1:
+        sLinePrevious = data[sLineNumber-2]
+        sStartNumPrev = None
+                
+        for i, sPoint in enumerate(sLinePrevious[sStartStarPos-1:sEndStarPos+2]): #sEndNumberPos + 1 doesnt include the last one here
+            if sPoint.isdigit():
+                sNum, sStartNum, sEndNum = findNumber(sLineNumber-1, sStartStarPos + i - 1)
+                if sStartNum != sStartNumPrev:
+                    sNumbers.insert(len(sNumbers), sNum)
+                    sStartNumPrev = sStartNum
+                    
+    # Look in current line if there is a symbol
+    sLine = data[sLineNumber-1]
+       
+    if sLine[sStartStarPos-1].isdigit():
+        sNum, sStartNum, sEndNum = findNumber(sLineNumber, sStartStarPos-1)
+        sNumbers.insert(len(sNumbers), sNum)
+        sStartNumPrev = sStartNum
+
+    if sLine[sEndStarPos+1].isdigit():
+        sNum, sStartNum, sEndNum = findNumber(sLineNumber, sStartStarPos+1)
+        sNumbers.insert(len(sNumbers), sNum)
+        sStartNumPrev = sStartNum
+
+    # Next line:
+    if sLineNumber < lengthDataset:
+        sLineNext = data[sLineNumber]
+        sStartNumPrev = None
+
+        for i, sPoint in enumerate(sLineNext[sStartStarPos-1:sEndStarPos+2]): #sEndNumberPos + 1 doesnt include the last one here
+            if sPoint.isdigit():
+                sNum, sStartNum, sEndNum = findNumber(sLineNumber+1, sStartStarPos + i - 1)
+                if sStartNum != sStartNumPrev:
+                    sNumbers.insert(len(sNumbers), sNum)
+                    sStartNumPrev = sStartNum
+
+    return sNumbers
+    
 # Loop over every token. Everytime we find a number we search how big it is and find the position. 
 # After that we search for if in the positions around, there are other characters the numbers of .
 
@@ -71,24 +143,34 @@ lineNumber = 0
 totalNumber = 0
 for line in data:
     lineNumber += 1
-    print(lineNumber)
+#    print(lineNumber)
     i = 0
     lineLength = len(line)
 
 #    print(line)
     while i < lineLength:
         point = line[i]
-
 #        print(point)
 
-        if point.isdigit():
-            startNumberPos = i
-            # Find the whol number including the position
-            number, endNumberPos, i = findNumber(lineNumber, i) #str(point) #functie nummer
-            symbol = findSymbol(lineNumber, startNumberPos, endNumberPos)
+        #if point.isdigit():
+         #   startNumberPos = i
+         #   # Find the whole number including the position
+         #   number, startNumberPos, endNumberPos = findNumber(lineNumber, i) # functie nummer
+         #   i = endNumberPos
+         #   symbol = findSymbol(lineNumber, startNumberPos, endNumberPos)
 
-            if symbol:
-                totalNumber += number
+         #   if symbol:
+         #       totalNumber += number
+        
+        if point == '*':
+            starPos = i
+            numbers = []
+            numbers = findNumbersStar(lineNumber, starPos)
+            print(numbers)
+            if len(numbers) == 2:
+            #    totalNumber -= numbers[0]
+            #    totalNumber -= numbers[1]
+                totalNumber += numbers[0]*numbers[1]
 
         i += 1
 
