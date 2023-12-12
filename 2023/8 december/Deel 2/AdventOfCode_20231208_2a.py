9734a1e2618ae5fbc106289ab9ab0a016c3984c1
bb288 <118565364+bb288@users.noreply.github.com>
2023-12-12 16:17:30 +0100

    Update AdventOfCode_20231208_2a.py now works

--- 2023/8 december/Deel 2/AdventOfCode_20231208_2a.py
+++ 2023/8 december/Deel 2/AdventOfCode_20231208_2a.py
@@ -1,5 +1,11 @@
-data_bestand = open("2023/8 december/Data/Input_20231208.txt")
-#data_bestand = open("2023/8 december/Data/Input_test.txt")
+from math import lcm
+
+#data = "test"
+data = "real"
+if data == "test":
+    data_bestand = open("8 december/Data/Input_20231208.txt")
+elif data == "real":
+    data_bestand = open("8 december/Data/Input_20231208.txt")
 data = data_bestand.read().split('\n') # Use split on \n instead of readlines(). This makes th \n dissapear 
 
 path = data[0]
@@ -48,60 +54,36 @@ def allNodesEndsZ(nodes, num):
 # (until it is the same as an earlier code + same step in line) and at what point there is 'Z' at the end. 
 # With this we can calculate the minimum where each starting node has a 'Z' at the end
 
+# dictionarty for loops. In here we will add for each starting node all the nodes_currentstep which end on a 'Z' 
+loopsZ = {}
+loopsLength = {}
+#startingNodes = startingNodes[0:2]
+
 for node in startingNodes:
-    nodeLoop = {}
     numberOfSteps = 0
+    nodeLoop = {}
     sContinue = 1
+    loopsZ[node] = []
     currentNode = node
+    currentStep = -1
     while sContinue == 1:
         numberOfSteps += 1
-#        print(currentNode, numberOfSteps)
+        prevNode = currentNode
+        prevStep = currentStep
         currentNode, currentStep = findNextNode(currentNode, numberOfSteps)
-#        print(currentNode, currentStep)
         if keyInDict(currentNode + "_" + str(currentStep), nodeLoop):
             lastNode = currentNode + "_" + str(currentStep)
             sContinue = 0
         else:
             nodeLoop[currentNode + "_"+ str(currentStep)] = numberOfSteps
             sContinue = 1
-#        elif currentNode.endswith('Z'):
-#            nodeLoop[currentNode + "_" + str(currentStep)] = 1
-#            sContinue = 1
-#        else:
-#            nodeLoop[currentNode + "_" + str(currentStep)] = 0
-#            sContinue = 1
-    lengthLoop = len(nodeLoop) - nodeLoop[lastNode] -1 #?
-#    print(nodeLoop)
-    print(nodeLoop.keys())
-    print(len(nodeLoop))
-    print(lastNode)
-    print(nodeLoop[lastNode])
-    print(lengthLoop)
+        if currentNode.endswith('Z'):
+            loopsZ[node] += (currentNode + "_" + str(currentStep), numberOfSteps)
+            sContinue = 1
 
+    lengthLoop = len(nodeLoop) - nodeLoop[lastNode] + 1
+    loopsLength[node] = lengthLoop
 
-'''
-currentNodes = startingNodes
-numberOfSteps = 0
-sContinue = 1
-while sContinue == 1:
-    numberOfSteps += 1
-#    print(numberOfSteps)
-    currentNodes = findNextNodes(currentNodes, numberOfSteps)
-#    for i, node in enumerate(currentNodes):
-#        currentNodes[i] = findNextNode(node, numberOfSteps)
-#    print(currentNodes)
-    if allNodesEndsZ(currentNodes, numberOfSteps):
-        sContinue = 0
-print(currentNodes)
-print(numberOfSteps)        
-
-currentNode = startingNode
-numberOfSteps = 0
-while currentNode != 'ZZZ':
-    numberOfSteps += 1
-    currentNode = findNextNode(currentNode, numberOfSteps)
-#    print(currentNode)
-
-print(numberOfSteps)
-
-'''
+# Find the number where every count is the same
+# Use LCM to calculate
+print(lcm(loopsZ['BBA'][1], loopsZ['BLA'][1], loopsZ['AAA'][1], loopsZ['NFA'][1], loopsZ['DRA'][1], loopsZ['PSA'][1]))
