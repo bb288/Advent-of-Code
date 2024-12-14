4da4d944f8049fb10b186247cd99519f256f56e0
Bas Borgs <basborgs@msn.com>
2024-12-14 09:48:23 +0100

    update 11 december 2024 part 1

--- 2024/11 december/Deel 1/AdventOfCode_11122024_1.py
+++ 2024/11 december/Deel 1/AdventOfCode_11122024_1.py
@@ -3,27 +3,22 @@ data_bestand = open("2024\\11 december\\Data\\Input_11122024.txt", 'r')
 #data_bestand = open("2024\\11 december\\Data\\Input_11122024_test.txt", 'r')
 data = data_bestand.read().split()
 
-print(data)
+#print(data)
 
 def blink(numberOfBlinks, number):
     if numberOfBlinks >= 25:
         return 1
-#    print("start Rock: " + str(number) + " en blinks = " + str(numberOfBlinks))
     lengthNumber = len(str(number))
 
     if number == 0:
-#        print(1)
         rocks = blink(numberOfBlinks + 1, 1)
     elif lengthNumber % 2 == 0:
         middle = int(lengthNumber / 2)
         leftRock = int(str(number)[0:middle])
-#        print(leftRock)
         rightRock = int(str(number)[middle:])
-#        print(rightRock)
         rocks = blink(numberOfBlinks + 1, leftRock) 
         rocks += blink(numberOfBlinks + 1, rightRock) 
     else:
-#        print(number * 2024)
         rocks = blink(numberOfBlinks + 1, number * 2024)
     return rocks
 
@@ -33,7 +28,3 @@ for stone in data:
     numberOfRocks += blink(0, int(stone))
 
 print(numberOfRocks)
-
-#print(blink(0, 125))
-
-#print(numberOfRocks)
